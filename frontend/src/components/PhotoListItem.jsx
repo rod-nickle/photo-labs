bf92cf8a5ae4a454a161e188efb14e15b166c417
rod-nickle <rod@bitwise-it.com>
2024-11-27 16:30:23 -0700

    apply styling to the PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,13 +8,26 @@ const PhotoListItem = (props) => {
   const photoListItem = { ...props.photoListItem };
   // console.log("PhotoListItem", photoListItem);
   return (
-    <div className="photo-list__item">
-      <h2>PhotoListItem Component</h2>
-      <img className="photo-list__image" src={photoListItem.imageSource} alt="Photo" ></img>
-      <img className="photo-list__user-profile" src={photoListItem.profile} alt="Profile" ></img>
-      <p className="photo-list__user-info"> {photoListItem.username}</p>
-      <p className="photo-list__user-location"> {photoListItem.location.city}, {photoListItem.location.country}</p>
-    </div>
+    // <li key="photoListItem.id" className="photo-list__item">
+    //   <img className="photo-list__image" src={photoListItem.imageSource} alt="Photo" ></img>
+    //   <div className="photo-list__user-details">
+    //     <img className="photo-list__user-profile" src={photoListItem.profile} alt="Profile" ></img>
+    //     <section>
+    //       <p className="photo-list__user-info"> {photoListItem.username}</p>
+    //       <p className="photo-list__user-location"> {photoListItem.location.city}, {photoListItem.location.country}</p>
+    //     </section>
+    //   </div>
+    // </li>
+    <figure key={photoListItem.id} className="photo-list__item">
+      <img className="photo-list__image" src={photoListItem.imageSource} alt="Photo" />
+      <figcaption className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={photoListItem.profile} alt="Profile picture of {photoListItem.username}" />
+        <section>
+          <p className="photo-list__user-info">{photoListItem.username}</p>
+          <p className="photo-list__user-location"> {photoListItem.location.city}, {photoListItem.location.country} </p>
+        </section>
+      </figcaption>
+    </figure>
   )
 };
 
